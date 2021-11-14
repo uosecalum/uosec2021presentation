76ce58e1ecd25837241e2c3ea77453033da13777
uosecalum <uosecalum@gmail.com>
2021-11-14 13:19:36 -0800

    adding challenge and solution

---
+++ secure.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+	printf("Enter the secure passphrase: \n");
+	char * buf = NULL;
+	size_t bufsize = 0;
+	ssize_t guess_size = 0;
+	guess_size = getline(&buf, &bufsize, stdin);
+	printf("test enter\n");
+	if (guess_size > 33) {
+		puts("Whoa, we said secure passphrase, not super secure, who uses a password over 32 chars I mean really now.\n");
+		return -1;
+	}
+	if (buf[guess_size-1] == '\n') {
+		buf[guess_size-1] = '\0';
+	}
+	int guess = strcmp(buf, "f8a9da56bc463ca06ebcde0cde409196");
+	if (guess == 0) {
+		printf("flag{uosec_rip_kb}.\n");
+		return 0;
+	}
+	else if (guess < 0 && guess > -10) {
+		printf("Now what was that dang passcode, this has got to be Mike Belotti's fault %d.\n", guess);
+	}
+	else if (guess > 0 && guess > 10) {
+		printf("Did it rhyme with Deschutes? I don't know, but I did spend entirely too long of my life in there %d.\n", guess);
+	}
+	else if (guess < 50 && guess > 10) {
+		printf("This reminds me of that time I left my wallet on the 4th floor of the Willamette building %d.\n", guess);
+	}
+	else if (guess > -50 && guess < -10) {
+		printf("Does everyone still wonder how it gets so hot in Deady hall basement %d.\n", guess);
+	}
+	else {
+		printf("Maybe these numbers mean something? hmmm %d.\n", guess);
+	}
+	return 0;
+}
---
+++ solution.py
@@ -0,0 +1,29 @@
+from pwn import *
+import sys
+
+context.log_level="critical"
+target = "localhost"
+port = 5000
+
+
+solution = []
+guess = False
+while True:
+    r = remote(target, port)
+    # chomp banner
+    r.recvline()
+    if guess:
+        guess = False
+        # try solution
+        r.sendline("".join(solution).encode('utf-8'))
+        try:
+            recv = r.recvline_contains(b'flag')
+            if recv:
+                print(recv.decode('utf-8').strip('\r'))
+                sys.exit(0)
+        except EOFError:
+            pass
+    r.sendline(''.join(solution).encode('utf-8')+chr(0).encode('utf-8'))
+    resp = r.recvuntil(b'.', drop=True).decode("ascii").split(' ')[-1]
+    solution.append(chr(0 - int(resp)))
+    guess = True
